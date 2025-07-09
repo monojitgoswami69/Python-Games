98ecd4d69d8d4e4edbb31d4c64b1929405649bc0
Monojit Goswami <monojit.goswami.0905@gmail.com>
2025-07-09 16:13:12 +0530

    Enhanced readability

--- RPS/RockPaperScissors.py
+++ RPS/RockPaperScissors.py
@@ -1,4 +1,5 @@
 import time, random, os
+# define function for printing with typewriter_effect
 def typewriter_print(sample):
     for line in sample.splitlines():
         for i in range(len(line)):
@@ -6,8 +7,10 @@ def typewriter_print(sample):
             time.sleep(0.01)
         print(line)
         time.sleep(0.5)  
+# define clear buffer function
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+# define function for fetching Current scoreboard 
 def scoreboard():
     return f"""
 Round: {rounds+1}
@@ -15,22 +18,27 @@ Round: {rounds+1}
 You: {user_score}
 Computer: {computer_score}
 """
+# define function for printing 3 second countdowns
 def countdown3s(text):
     for i in range(3,0,-1):
         for j in range(4):
             print(f"{text} {i}{"."*j}    ", end="\r", flush=True)
             time.sleep(1/4)
+# =========== EXECUTION BEGINS ============
+# clear buffer before execution
 clear_screen()
+# define code params
 user_score = computer_score = rounds = 0
 choice_list = ["rock 🪨 ", "paper 📄", "scissors ✂️ "]
+# define and print first header texts
 header1 = f"""
 Welcome to our interactive game of Rock, Paper, Scissors!\n
 Rules of the game-
 User has to choose among Rock/Paper/Scissors and compete with the computer
    winning criteria-
-   {choice_list[0]} beats {choice_list[2]}
-   {choice_list[1]} beats {choice_list[0]}
-   {choice_list[2]} beat {choice_list[1]}
+   {choice_list[0]} beats {choice_list[2]} [Rock breaks Scissors]
+   {choice_list[1]} beats {choice_list[0]} [Paper wraps Rock]
+   {choice_list[2]} beat {choice_list[1]}  [Scissors shred paper to pieces]
 Good Luck! Hope you enjoy our game!!
 """
 header2 = """
@@ -46,19 +54,24 @@ time.sleep(1)
 typewriter_print(scoreboard())
 time.sleep(0.5)
 typewriter_print(header2)
+# ===== PRIMARY LOOP =====
 while True:
+    # print static headers including current scoreboard
     clear_screen()
     print(header1, end="")
     print(scoreboard(), end="")
     print(header2)
     user_input = input(">>>")
+    # validate input
     while user_input not in "123qQ":
         print("Invalid choice!")
         user_input = input(">>>")
+    # check quit status
     if user_input in "qQ":
         print()
         countdown3s("Quitting")
         break  
+    # define current iteration choices
     computer_choice = choice_list[random.randint(0,2)]
     user_choice = choice_list[int(user_input)-1]
     time.sleep(1)
@@ -66,6 +79,7 @@ while True:
     time.sleep(0.5)
     typewriter_print(f"Computer chose {computer_choice}")
     time.sleep(0.5)
+    # check result and update score
     if user_choice == computer_choice:
         round_result = "\033[33mIt's a tie!\033[0m"
         user_score+=1
@@ -76,21 +90,25 @@ while True:
     else:
         round_result = "\033[31mYou Lose!\033[0m"       
         computer_score+=1
+    # print result
     print(f"{round_result}\n")
     time.sleep(1)
     rounds+=1
     countdown3s("Next round in")
     clear_screen()
+# ====== GAME END ======
 clear_screen()
 time.sleep(0.5)
 typewriter_print("----- GAME END -----\n")
 time.sleep(0.5)
+# print game statistics if played 
 if rounds>0:
     typewriter_print(f"""\n\033[1mGame statistics-\033[0m
 Rounds played: {rounds}
 Your Score: {user_score}
 Computer's Score: {computer_score}   
     """)
+    # check and print Series result
     if user_score == computer_score:
         game_result = "\033[33mThe Series is a Draw!\033[0m"
     elif user_score>computer_score:
@@ -99,6 +117,7 @@ Computer's Score: {computer_score}
         game_result = "\033[31mAlas!! You Lose the Series!\033[0m"
     time.sleep(0.5)
     typewriter_print(game_result)
+# roast user if not played 
 else:
     typewriter_print("""
 OH man you didn't even try!
